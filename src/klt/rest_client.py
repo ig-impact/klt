021e7fec6a0cd70fc787d7392a9c0c2184b11156
Iyed GHEDAMSI <iyed.ghedamsi@impact-initiatives.org>
2025-10-29 10:31:56 +0300

    accept a custom session

--- src/klt/rest_client.py
+++ src/klt/rest_client.py
@@ -1,9 +1,13 @@
 from dlt.sources.helpers.rest_client.auth import APIKeyAuth
 from dlt.sources.helpers.rest_client.client import RESTClient
 from dlt.sources.helpers.rest_client.paginators import JSONLinkPaginator
+from requests import Session
+from requests_cache import CachedSession
 
 
-def make_rest_client(kobo_token: str, kobo_server: str) -> RESTClient:
+def make_rest_client(
+    kobo_token: str, kobo_server: str, session: Session | CachedSession | None = None
+) -> RESTClient:
     return RESTClient(
         base_url=kobo_server,
         auth=APIKeyAuth(
@@ -11,4 +15,5 @@ def make_rest_client(kobo_token: str, kobo_server: str) -> RESTClient:
         ),
         paginator=JSONLinkPaginator(next_url_path="next"),
         data_selector="results",
+        session=session,
     )
